c19e77d7e07281419a22687da3cfcf267c72483c
kohlsjw3656 <kohlsjw3656@gmail.com>
2020-05-14 01:35:16 -0500

    Now displays country name

--- src/reducer.js
+++ src/reducer.js
@@ -3,9 +3,9 @@ import { Action } from "./actions";
 const initialState = {
 	isWaiting: false,
 	purchases: [],
-	countries: [{c_id: 0, c_Name: "Germany", ipcs: 30},
-							{c_id: 1, c_Name: "Soviet Union", ipcs: 37}],
-	country: {c_id: 0, c_Name: "Germany", ipcs: 30},
+	countries: [{c_id: 0, c_name: "Germany", ipcs: 30},
+							{c_id: 1, c_name: "Soviet Union", ipcs: 37}],
+	country: {c_id: 0, c_name: "Germany", ipcs: 30},
 	turn: 1,
 	dateYear: 1940,
 	seasons: ["Spring/Summer", "Fall/Winter"],
